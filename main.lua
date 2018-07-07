b699ee9b6960def597133b9f957f2498ad9e43d0
Christopher Stokes <christopherstokes@gmail.com>
2018-07-07 11:42:18 -0700

    initial commit

---
+++ main.lua
@@ -0,0 +1,24 @@
+local Entity = require "entity"
+
+local player = Entity
+player:init(400, 300, 10, {255, 0, 0})
+
+-- preload function to load any images/sounds or the like
+function love.load()
+end
+
+-- update function called continuously
+function love.update(dt)
+  player:update(dt)
+end
+
+-- draw function called 60 times/sec typically
+function love.draw()
+--  love.graphics.print("Hello World", 400, 300)
+--  love.graphics.clear(0,0,0)
+  player:draw()
+end
+
+-- quit function called when player exits game
+function love.quit()
+end
