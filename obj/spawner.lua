12161fc384084c959b9e5fa74ff436ce258420a6
Christopher Stokes <christopher.stokes@dealerspike.com>
2018-07-11 10:24:55 -0700

    add custom spawner init and update

--- obj/spawner.lua
+++ obj/spawner.lua
@@ -1,3 +1,14 @@
 local Spawner = Entity:extend("Spawner")
 
+function Spawner:init(x, y, r, col, period)
+    Spawner.super:init(x, y, r, col)
+    self.period = period
+end
+
+function Spawner:update(dt)
+    if dt % period == 0 then
+        
+    end
+end
+
 return Spawner
