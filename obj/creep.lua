5fa0637ae10680e60453f5d7da73790d36c24417
Christopher Stokes <christopherstokes@gmail.com>
2018-07-13 05:28:49 -0700

    add Creep init as well as base 3 levels of simple creep stats

--- obj/creep.lua
+++ obj/creep.lua
@@ -1,5 +1,34 @@
 local Creep = Entity:extend("Creep")
 
+local level_3 = {
+    hp = 4,
+    atk = 1,
+    range = 1,
+    spd = 2
+}
+
+local level_2 = {
+    hp = 3,
+    atk = 1,
+    range = 0.5,
+    spd = 2
+}
+
+local level_1 = {
+    hp = 2,
+    atk = 0.5,
+    range = 0.5,
+    spd = 3
+}
+
+function Creep:init(x, y, r, col)
+    Creep.super.init(self, x, y, r, col)
+
+    for k, v in ipairs(level_3) do
+        self[k] = v
+    end
+end
+
 function Creep:update(dt)
     
 end
