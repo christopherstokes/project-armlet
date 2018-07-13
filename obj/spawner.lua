d72ab579ea1bd4ba28ba9f726824876672aff0f5
Christopher Stokes <christopherstokes@gmail.com>
2018-07-13 05:28:08 -0700

    add spawner and base period length between spawning

--- obj/spawner.lua
+++ obj/spawner.lua
@@ -2,12 +2,12 @@ local Spawner = Entity:extend("Spawner")
 
 function Spawner:init(x, y, r, col, period)
     Spawner.super:init(x, y, r, col)
-    self.period = period
+    self.period = period or 4
 end
 
 function Spawner:update(dt)
     if dt % period == 0 then
-        
+        -- insert creep in some way
     end
 end
 
