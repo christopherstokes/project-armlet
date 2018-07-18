70593766a5ac72cb86b004826b10f8b4f3742ffc
Christopher Stokes <christopherstokes@gmail.com>
2018-07-17 21:31:23 -0700

    update spawner logic to reflect the global and entity timers

--- obj/spawner.lua
+++ obj/spawner.lua
@@ -2,14 +2,13 @@ local Spawner = Entity:extend("Spawner")
 
 function Spawner:init(x, y, r, col, period)
     Spawner.super.init(self, x, y, r, col)
-    self.period = period or 5
-    self.timer = love.timer.getTime()
+    self.period = period or 120
 end
 
 function Spawner:update(dt)
     -- self.debug = "updated at: "..dt
     -- self.debug = love.timer.getTime() - self.timer
-    if (love.timer.getTime() - self.timer) % self.period <= 0.02 then
+    if (timer - self.timer) % self.period == 0 then
         -- insert creep in some way
         Creep:new("BasicCreep", 3, self.x, self.y)
         -- self.debug = ""..dt
