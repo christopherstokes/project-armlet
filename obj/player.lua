c078594446e57b8a9b6553268a50678a20251b33
Christopher Stokes <christopher.stokes@dealerspike.com>
2018-07-18 09:17:02 -0700

    add keyboard controls

--- obj/player.lua
+++ obj/player.lua
@@ -6,10 +6,10 @@ function Player:init(x, y, r, col, spd, joystick)
 end
 
 function Player:update(dt)
-    if self.joystick:isGamepadDown("dpup") and self.y > 0 then self.y = self.y - self.spd end
-    if self.joystick:isGamepadDown("dpdown") and self.y < love.graphics.getHeight() then self.y = self.y + self.spd end
-    if self.joystick:isGamepadDown("dpleft") and self.x > 0 then self.x = self.x - self.spd end
-    if self.joystick:isGamepadDown("dpright") and self.x < love.graphics.getWidth() then self.x = self.x + self.spd end
+    if self.joystick:isGamepadDown("dpup") or love.keyboard.isDown("up") and self.y > 0 then self.y = self.y - self.spd end
+    if self.joystick:isGamepadDown("dpdown") or love.keyboard.isDown("down") and self.y < love.graphics.getHeight() then self.y = self.y + self.spd end
+    if self.joystick:isGamepadDown("dpleft") or love.keyboard.isDown("left") and self.x > 0 then self.x = self.x - self.spd end
+    if self.joystick:isGamepadDown("dpright") or love.keyboard.isDown("right") and self.x < love.graphics.getWidth() then self.x = self.x + self.spd end
 end
 
 return Player
